a3e8c568e9fe4d4df1350b0ab6968d2f43e4f205
mohamed.hammam <mohamed.hammam@wecancity.org>
2023-08-11 16:46:25 +0300

    Initial Commit

---
+++ WTMP/WTMPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTMPApp.swift
+//  WTMP
+//
+//  Created by wecancity on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WTMPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
